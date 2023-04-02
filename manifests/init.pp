2b5c2da829caa6fba8597bac1a0cedc12381ca7c
vitaly.by <vitaly.by@gmail.com>
2023-04-02 13:05:48 +0300

    dz4

---
+++ manifests/init.pp
@@ -0,0 +1,57 @@
+class slave1 {
+  package { 'httpd':
+    ensure => installed,
+  }
+
+  service { 'httpd':
+    ensure  => true,
+    enable  => true,
+    require => Package['httpd'],
+  }
+
+  file { '/var/www/html/index.html':
+          ensure => file,
+          source => 'https://raw.githubusercontent.com/vitalyby/devops-hometasks/main/03-puppet/index.html',
+  }
+
+  service { 'firewalld':
+    ensure  => 'stopped',
+    enable  => false,
+  }  
+
+}
+
+class slave2 {
+  package { 'httpd':
+    ensure => installed,
+  }
+    package { 'php':
+    ensure => installed,
+  }
+
+  service { 'httpd':
+    ensure  => true,
+    enable  => true,
+    require => Package['httpd'],
+  }
+
+  file { '/var/www/html/index.php':
+          ensure => file,
+          source => 'https://raw.githubusercontent.com/vitalyby/devops-hometasks/main/03-puppet/index.php',
+  }  
+
+  service { 'firewalld':
+    ensure  => 'stopped',
+    enable  => false,
+  }  
+
+  exec { 'edit config httpd.conf':
+    command => 'sed -i "s/^    DirectoryIndex index.html$/    DirectoryIndex index.php/g" /etc/httpd/conf/httpd.conf',
+    path => "/usr/bin",
+  }
+
+    exec { 'restart httpd':
+    command => '/bin/systemctl restart sshd.service'
+  }
+
+}
