35abb233b65be49443f211ce3e2d68a911829616
vitaly.by <vitaly.by@gmail.com>
2023-04-02 23:58:52 +0300

    dz4

--- manifests/site.pp
+++ manifests/site.pp
@@ -6,7 +6,19 @@ node 'slave1.puppet' {
    include slave1
 }
 
-node 'master.puppet' {}
+node 'master.puppet' {
+   include nginx
+
+   nginx::resource::server { 'html':
+   listen_port => 80,
+   proxy       => 'http://192.168.50.5',
+   }
+
+   nginx::resource::server { 'php':
+   listen_port => 81,
+   proxy       => 'http://192.168.50.6',
+   }   
+}
 
 node 'mineserver.puppet' {
    include mineserver
